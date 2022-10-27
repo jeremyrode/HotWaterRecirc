9d330d97994444fe464e80f2ab1a2803499fe623
jeremyrode <jrode@ziva-corp.com>
2022-10-27 09:55:49 -0700

    matlab changes

--- analysis/demandHistogram.m
+++ analysis/demandHistogram.m
@@ -2,6 +2,6 @@
 
 PosixTimeData = readmatrix('V:\DemandLogRecirc.txt','Delimiter',',');
 
-DataTimeData = datetime(PosixTimeData,'ConvertFrom','posixtime');
+DataTimeData = datetime(PosixTimeData,'ConvertFrom','posixtime','TimeZone','UTC');
 
-histogram(DataTimeData.Hour)
+histogram(mod(DataTimeData.Hour + 16,24) + DataTimeData.Minute/60)
